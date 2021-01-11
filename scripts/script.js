082f9194919d01f4d2366381f53955f536fc9e0e
Nino-dev <33895563+Nino-dev@users.noreply.github.com>
2021-01-11 16:22:32 +0100

    Created hamburger menu animation

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,23 @@ let radioButtons = document.getElementsByName('radio');
 
 window.onscroll = () => {
   if (document.documentElement.scrollTop > 74) {
-    document.querySelector('.hamburger').classList.add('fix-burger');
+    document.querySelector('.hamburger-menu').classList.add('fix-burger');
   } else {
-    document.querySelector('.hamburger').classList.remove('fix-burger');
+    document.querySelector('.hamburger-menu').classList.remove('fix-burger');
   }
+}
+
+const hamburger = document.querySelector('.hamburger-menu');
+
+// Toggle menu on
+hamburger.onclick = () => {
+  const nav = document.querySelector('nav');
+  nav.classList.add('show-nav');
+}
+
+// Toggle menu off
+const close = document.querySelector('.close-menu');
+close.onclick = () => {
+  const nav = document.querySelector('nav');
+  nav.classList.remove('show-nav');
 }
